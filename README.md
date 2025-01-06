08db8005f5b77d20ba902e34a535714ce78ff10d
AbrahamCoco <abraham_cryter2007@hotmail.com>
2025-01-06 13:19:45 -0600

    [Coco]Migration to spring boot

--- README.md
+++
@@ -1,5 +0,0 @@
-# Nutrilud
-
-Un sistema de gestión de nutrición para nutricionistas y pacientes, que permite llevar un control de las dietas y los pacientes.
-
-Tambien con un sistema de articulos para que los nutricionistas puedan publicar articulos de interes para sus pacientes.
---
+++ src/main/java/com/devconmx/nutrilud_backend/NutriludBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devconmx.nutrilud_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriludBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriludBackendApplication.class, args);
+	}
+
+}
